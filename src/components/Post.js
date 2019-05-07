c674dfddbced3f9edb737e755ad960abb39ee75e
Bander <bannadr1@hotmail.com>
2019-05-08 01:59:36 +0300

    action creators

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
-
+import { deletePost } from '../actions/postActions';
 class Post extends Component {
     handleClick = (e) => {
         this.props.deletePost(this.props.post.id);
@@ -36,7 +36,7 @@ const mapStateToProps = (state, ownProps) => {
 }
 const mapDispatchToProps = (dispatch) => {
     return {
-        deletePost: (id) => { dispatch({ type: 'DELETE_POST', id }) }
+        deletePost: (id) => { dispatch(deletePost(id)) }
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Post);
